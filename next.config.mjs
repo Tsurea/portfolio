e384283aa0e4308146a7dd127479c07d3a04586f
mariomario <yo@example.com>
2024-08-22 16:50:45 +0200

    🧠 only export in the next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	output: "export",
-	distDir: "build",
-	env: {
-		SERVER_HOSTNAME: process.env.NEXT_PUBLIC_SERVER_HOSTNAME,
-	},
-	images: {
-		domains: ["axrdwmkjjfqxhpmhabxn.supabase.co", "images.unsplash.com"]
-	},
-	i18n: {
-		// These are the locales you want to support
-		locales: ['en', 'fr'],
-		// This is the default locale you want to be used when visiting a non-locale prefixed path
-		defaultLocale: 'en',
-	},
+	output: "export"
 }
 
 export default nextConfig;
